bc074a09bc55fa4ad7f37aa63b2689efeaa99dd2
Drvinc <t24735281@gmail.com>
2018-04-12 01:12:37 +0800

    renderImage

--- js/topic_load.js
+++ js/topic_load.js
@@ -11,60 +11,19 @@ function renderTable (csv_path, table_id){
             .append("td")
             .text(function(d) { return d; });
   })  
-};
+}
 
-<!-- sort functionality for tables -->
-function sortTable(n, table_id) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById(table_id);
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc"; 
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++; 
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
+<!-- work with exif.js to load a jpg image with decription and reference -->
+function renderImage (img_id){
+  var img1 = document.getElementById(img_id);
+  EXIF.getData(img1, function() {
+    var Copyright = EXIF.getTag(this, "Copyright");
+    var PMID = Copyright.match(/\d+/g).map(Number);
+    var pubmedURL = "https://www.ncbi.nlm.nih.gov/pubmed/" + PMID + "/";
+    console.log(pubmedURL);
+    var ImgDescript = EXIF.getTag(this, "ImageDescription");
+    var ImgCaption = document.createElement("figcaption");
+    ImgCaption.innerHTML = ImgDescript + " (" + "<a href='"+pubmedURL+"'>" + Copyright + "</a>" + ")";
+    img1.parentNode.appendChild(ImgCaption);
+  });
 }
